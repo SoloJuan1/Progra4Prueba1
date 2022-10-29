178d7a1d5c24d2ef3ff96dacfb29c4c5c445c8cd
SoloJuan1 <jclmh@outlook.es>
2022-10-28 19:10:30 -0600

    Creando servidor http

--- tabla.js
+++ tabla.js
@@ -1,1 +1,14 @@
-// Codigo de la tabla de multiplicar
+// Iniciando servidor http
+const http= require('http')
+const host= "localhost"
+const port=80
+const server =http.createServer(function(peticion,respuesta){
+    respuesta.writeHead(200,{'Content-Type':'application/txt'})
+    respuesta.write('Probando servidor');
+    respuesta.end()
+})
+
+server.listen(80,host,function(error){
+    console.log(`Servidor Inicializado: http://${host}/${port}`)
+})
+
