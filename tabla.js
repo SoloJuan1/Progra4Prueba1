e2199264386cf501194242fc1b1328274d1b9486
SoloJuan1 <jclmh@outlook.es>
2022-10-28 19:14:56 -0600

    Arreglo servidor mal iniciado

--- tabla.js
+++ tabla.js
@@ -1,14 +1,14 @@
 // Iniciando servidor http
 const http= require('http')
 const host= "localhost"
-const port=80
+const port=8080
 const server =http.createServer(function(peticion,respuesta){
-    respuesta.writeHead(200,{'Content-Type':'application/txt'})
+    respuesta.writeHead(200,{'Content-Type':'text/plain'})
     respuesta.write('Probando servidor');
     respuesta.end()
 })
 
-server.listen(80,host,function(error){
-    console.log(`Servidor Inicializado: http://${host}/${port}`)
+server.listen(port,host,function(){
+    console.log(`Servidor Inicializado: http://${host}:${port}`)
 })
 
